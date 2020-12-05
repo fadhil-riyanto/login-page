ab7252b132101ac0f3ad13ed67120a6e3337ad0a
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-12-05 12:23:54 +0700

    create email form

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,17 +8,22 @@
 
     <!-- masukkan data dan javascript -->
     <script src="asset/vue.js"></script>
-	<script src="asset/jquery.js"></script>
-	<script src="asset/pooper.js"></script>
-	<script src="asset/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="asset/bootstrap.min.css">
+    <script src="asset/jquery.js"></script>
+    <script src="asset/pooper.js"></script>
+    <script src="asset/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="asset/bootstrap.min.css">
     <link rel="stylesheet" href="assets\custom-styling.css">
 
 </head>
+
 <body class='text-center'>
-   <form class='form-signin'action="">
-       <img src="assets\fadhil-circle.png" alt="image fadhil gambar" srcset="">
-       <h1 class='h3 mb-3 font-weight-normal'>Please Sign in</h1>
-   </form>
+    <form class='form-signin' action="">
+        <img src="assets\fadhil-circle.png" alt="image fadhil gambar" srcset="">
+        <h1 class='h3 mb-3 font-weight-normal'>Please Sign in</h1>
+        <label for="inputEmail">Email adress</label>
+        <input type="email" class='form-control' placeholder="Email Adress" required autofocus id='inputEmail'
+            name="email" id="">
+    </form>
 </body>
+
 </html>

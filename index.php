e1ed81e3ed4ddd352a41e49ed5cd64ce644440c5
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-12-05 12:27:41 +0700

    add password field and edit path. sorry,, wrong...xixix

--- index.php
+++ index.php
@@ -7,22 +7,31 @@
     <title>Login Page - Dev By Fadhil Riyanto</title>
 
     <!-- masukkan data dan javascript -->
-    <script src="asset/vue.js"></script>
-    <script src="asset/jquery.js"></script>
-    <script src="asset/pooper.js"></script>
-    <script src="asset/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="asset/bootstrap.min.css">
+    <script src="assets/vue.js"></script>
+    <script src="assets/jquery.js"></script>
+    <script src="assets/pooper.js"></script>
+    <script src="assets/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
     <link rel="stylesheet" href="assets\custom-styling.css">
 
 </head>
 
 <body class='text-center'>
     <form class='form-signin' action="">
-        <img src="assets\fadhil-circle.png" alt="image fadhil gambar" srcset="">
+        <img class="mb-4" src="assets\fadhil-circle.png" width="72" height="72" alt="image fadhil gambar" srcset="">
         <h1 class='h3 mb-3 font-weight-normal'>Please Sign in</h1>
-        <label for="inputEmail">Email adress</label>
+        <label for="inputEmail" class="sr-only">Email adress</label>
         <input type="email" class='form-control' placeholder="Email Adress" required autofocus id='inputEmail'
-            name="email" id="">
+            name="email">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <p class="mt-5 mb-3 text-muted">© 2020 - Fadhil Riyanto</p>
     </form>
 </body>
 

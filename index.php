b15b3e881fe6ec9e2fe2a07f3f917eb3b9275f98
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-12-05 12:16:00 +0700

    add static files

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page - Dev By Fadhil Riyanto</title>
+
+    <!-- masukkan data dan javascript -->
+    <script src="asset/vue.js"></script>
+	<script src="asset/jquery.js"></script>
+	<script src="asset/pooper.js"></script>
+	<script src="asset/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="asset/bootstrap.min.css">
+    <link rel="stylesheet" href="assets\custom-styling.css">
+
+</head>
+<body>
+   
+</body>
+</html>

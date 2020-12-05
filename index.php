77dad7068f8962f37e8df9bb9a83bc16cb0399c3
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-12-05 12:28:29 +0700

    delete checkbok

--- index.php
+++ index.php
@@ -25,11 +25,7 @@
             name="email">
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
-        </div>
+
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
         <p class="mt-5 mb-3 text-muted">© 2020 - Fadhil Riyanto</p>
     </form>
